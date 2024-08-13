d5349c93149661a46b97472821a66487ea5a182b
Ramlatch <info@robinmaurer.com>
2024-08-13 15:33:33 +0200

    fix missing $

--- scaling_experiments.bash
+++ scaling_experiments.bash
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-#for each in (1,2,4,8,16,32) start a sbatch
-#SBATCH --output="/hkfs/work/workspace/scratch/ie5012-MA/results/scaling_experiments/s.out"
 BASE_DIR="/hkfs/work/workspace/scratch/ie5012-MA"
 OUTS="$BASE_DIR/results/scaling_experiments"
-for NUM_NODES in 1 2 4 8 16 32
-do
+for NUM_NODES in 1 2 4 8 16 32; do
   export CONFIG_FILE="${BASE_DIR}/FourCastNet/config/AFNODIST$NUM_NODES.yaml"
-  jobID=(sbatch --job-name="Scal_$NUM_NODES" --nodes=$NUM_NODES --output="$OUTS/scale_$NUM_NODES.out" FourCastNet/scaling_experiments_worker.sh 2>&1 | sed 's/[S,a-z]* //g')
+  jobID=$(sbatch --job-name="Scal_$NUM_NODES" --nodes=$NUM_NODES --output="$OUTS/scale_$NUM_NODES.out" FourCastNet/scaling_experiments_worker.sh 2>&1 | sed 's/[S,a-z]* //g')
   sbatch --dependency=afterok:${jobID} --job-name="Fine_$NUM_NODES" --nodes=$NUM_NODES --output="$OUTS/scale_$NUM_NODES.finetune.out" FourCastNet/scaling_experiments_finetune.sh
 done
